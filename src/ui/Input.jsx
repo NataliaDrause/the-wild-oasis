3403a77f5e9f47f63237129f1e8de538196c773f
NataliaDrause <nataliadrause@gmail.com>
2025-02-11 20:07:26 +0200

    add form and handling with React Hook Form

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -5,7 +5,7 @@ const Input = styled.input`
   background-color: var(--color-grey-0);
   border-radius: var(--border-radius-sm);
   box-shadow: var(--shadow-sm);
-  padding: 08rem 1.2rem;
+  padding: 0.8rem 1.2rem;
 `;
 
 export default Input;
